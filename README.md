6269dba00b1151d79b7a060a915c35e83e3250d3
Miley Wright <dishaliekwright@yahoo.com>
2020-02-04 19:57:29 -0500

    Importing Files

---
+++ README.md
@@ -0,0 +1,38 @@
+# International-School-Social-Worker-3-buildweek
+
+
+The base URL for this project is: https://intl-school-social-worker.herokuapp.com/
+International School Social Worker is a website that will help allow administrators and social workers insert students data. 
+
+You can checkout our marketing page here: 
+
+Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+Installing
+
+A step by step series of examples that tell you how to get a development env running
+
+Install the dependancies that comes with the repo which include:
+
+-Knex
+-knex-cleaner
+-Node
+-nodemon
+-bcrypt
+-dotenv
+pg
+-cors
+express
+helmet
+SQLite3
+jsonWebToken
+Jest
+Supertest
+
+npm i
+
+Start the development local server, this git will use port 5000
+
+npm run server
---
+++ knexfile.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: { filename: './database/users.db3' },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations',
+      tableName: 'dbmigrations',
+    },
+    seeds: { directory: './database/seeds' },
+  },
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/test.db3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {  directory: './database/seeds',
+    },
+  },
+
+  production: {                                                        
+    client: 'pg',                                                                                                  
+    connection: process.env.DATABASE_URL,                               
+    pool: {                                                            
+      min: 2,                                                           
+      max: 10,                                                          
+    },                                                                  
+    migrations: {                                                       
+        directory: './database/migrations',                                 
+    },                                                                  
+    seeds: {                                                            
+      directory: './database/seeds',                                        
+    },                                                                  
+  },
+};
